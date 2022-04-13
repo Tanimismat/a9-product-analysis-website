6e6377a7aeb93634878c1d5574e9ae33ad0568f1
Tanimismat <tanim.ismat@gmail.com>
2022-04-13 10:09:33 +0600

    custome hook

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -4,7 +4,7 @@ const Blogs = () => {
     return (
         <div>
             <h2>What is context API? What is the purpose of it?</h2>
-            <p>Context API allows to share state or data which is considered as "global" for a tree of react components. Context API is needed when the component tree become bigger. This technique is use to avoid props drilling(process of sending down props from a higher level component to lower level component) and make the code cleaner. There are two main steps to use context API , (1) Setup a context provider and define the data which have to be shared. (2)Using a context consumer when the data have to be retrived.   </p>
+            <p>Context API allows to share state or data which is considered as "global" for a tree of react components. Context API is needed when the component tree become bigger. This technique is use to avoid props drilling(process of sending down props from a higher level component to lower level component) and make the code cleaner. There are two main steps to use context API , (1) Setup a context provider and define the data which have to be shared. (2)Using a context consumer when the data have to be retrieved.   </p>
             <h2>What is semantic tag?</h2>
             <p>Semantic tags are one of the most important features of HTML5. these tags refers to syntax that makes the HTML more understandable by defining different sections. It makes web pages more informative and adaptable to browser and search engines. Example: {'<section>'}, {'<picture>'}, {'<main>'}, {'<table>'} tag etc.</p>
         </div>
--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import useReviews from '../hooks/useReviews';
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useReviews();
+
     return (
         <div>
-            <h2>Reviews page</h2>
+            <h2>Reviews page: {reviews.length}</h2>
         </div>
     );
 };
