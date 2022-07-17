e78143e8118584f031877c790af76e810eeb5528
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-17 12:34:08 +0600

    image added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ThreeReviews from './ThreeReviews';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import ThreeReviews from "./ThreeReviews";
+import image1 from "../images/acer-swift.png";
 
 const Home = () => {
+	return (
+		<div className="px-3">
+			<div className="d-flex align-items-center justify-content-center">
+				<div className="col-md-7">
+					<h2 className="brand">
+						{" "}
+						<span className="fst-italic">acer</span>{" "}
+						<span className="product-name">Swift X</span>
+					</h2>
+					<p className="text-secondary">
+						Unleash the Swift X on your next project. With extreme performance
+						condensed within a thin and light metal chassis, creation becomes
+						portable, and so do you. Featuring thermal solutions and a display
+						that’s made for makers.
+					</p>
+					<button className="button ">Live Demo</button>
+				</div>
+				<div className="col-md-4">
+					<img style={{ width: "350px" }} src={image1} alt="Product" />
+				</div>
+			</div>
+			<div>
+				<h5 className="my-5">Customer Reviews (3)</h5>
+				<ThreeReviews></ThreeReviews>
 
-
-    return (
-        <div>
-            <div className='d-flex align-items-center justify-content-center'>
-                <div className='col-md-7'>
-                    <h2 className='brand'> <span className='fst-italic'>acer</span> <span className='product-name'>Swift X</span></h2>
-                    <p className='text-secondary'>Unleash the Swift X on your next project. With extreme performance condensed within a thin and light metal chassis, creation becomes portable, and so do you. Featuring thermal solutions and a display that’s made for makers.</p>
-                    <button className='button '>Live Demo</button>
-                </div>
-                <div className='col-md-4'>
-                    <img src={"https://static.acer.com/up/Resource/Acer/Laptops/Swift_X_SFX14_51G/Images/20220407/acer-swift-x-sfx14-51g-FpBl-alga-green-modelpreview.png"} alt="" />
-                </div>
-            </div>
-            <div>
-                <h5 className='my-5'>Customer Reviews (3)</h5>
-                <ThreeReviews></ThreeReviews>
-
-                <button className='seeAll-button'><Link className='seeAll-btn my-5' to='/reviews'>See All Reviews</Link></button>
-
-            </div>
-        </div>
-    );
+				<button className="seeAll-button">
+					<Link className="seeAll-btn my-5" to="/reviews">
+						See All Reviews
+					</Link>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
--- src/Components/SingleReview.js
+++ src/Components/SingleReview.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const SingleReview = (props) => {
-    console.log(props)
-    const { name, img, ratings, review } = props.review
-    return (
-        <div className='single-review'>
-            <div className="col">
-                <div className="card h-100 ">
-                    <img src={img} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h6 className="card-title">{name}</h6>
-                        <p className="card-text">{review.slice(0, 155)} ... <span><small className='text-secondary'>see more</small></span></p>
-                        <p><small>Ratings: {ratings}</small></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	const { name, img, ratings, review } = props.review;
+	return (
+		<div className="single-review">
+			<div className="col">
+				<div className="card h-100 ">
+					<img src={img} className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h6 className="card-title">{name}</h6>
+						<p className="card-text">
+							{review.slice(0, 155)} ...{" "}
+							<span>
+								<small className="text-secondary">see more</small>
+							</span>
+						</p>
+						<p>
+							<small>Ratings: {ratings}</small>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SingleReview;
